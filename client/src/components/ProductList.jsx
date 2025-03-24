aecdc2d858cede1fa8d5a00cea34a188cb95709e
haon-wuchang <haoony@naver.com>
2025-03-24 14:40:28 +0900

    Update ProductList.jsx

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -12,10 +12,11 @@ export default function ProductList() {
     },[])
     //출력 리스트 생성 [ [{},{},{}], [{},{},{}], [{}]]
     const rows = [];
+    if(list){
     for(let i=0; i < list.length; i+=3){ // [{0},{1},{2}]  
         rows.push(list.slice(i, i+3));  // [{0},{1},{2}]
     }
-
+    }
     return (
         <div>
             {   
