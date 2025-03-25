3bfc312378001406b8fd0dab5590d4f520ed0782
haon <haoony@naver.com>
2025-03-25 10:05:04 +0900

    s

--- server/repository/db.js
+++ server/repository/db.js
@@ -4,10 +4,10 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: 'shoppy-redux.ct6mky2ksrs7.ap-northeast-2.rds.amazonaws.com',
     port: 3306,
-    user: 'root',
-    password: 'mysql1234',
+    user: 'admin',
+    password: 'swamp213!!',
     database: 'hrdb2019',
 });
 
